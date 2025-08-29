3193dba1f456516045939fa484982cf85553e040
10012 <nas7062@naver.com>
2025-08-29 23:42:05 +0900

    style : 카테고리 리스트 css 변경

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,19 +1,40 @@
-const Category = [
-  '인기',
-  '할인',
-  '상의',
-  '바지',
-  '아우터',
-  '신발',
-  '가방',
-  '패션소품',
+import hot from '@/assets/hot.png';
+import sale from '@/assets/sale.png';
+import top from '@/assets/top.png';
+import bottom from '@/assets/bottom.webp';
+import outer from '@/assets/outer.png';
+import shose from '@/assets/shoes.webp';
+import bag from '@/assets/bag.webp';
+import fashion from '@/assets/fashion.png';
+
+type CategoryType = {
+  name: string;
+  image: string;
+};
+
+const Category: CategoryType[] = [
+  { name: '인기', image: hot },
+  { name: '할인', image: sale },
+  { name: '상의', image: top },
+  { name: '바지', image: bottom },
+  { name: '아우터', image: outer },
+  { name: '신발', image: shose },
+  { name: '가방', image: bag },
+  { name: '패션소품', image: fashion },
 ];
 
 export default function CategoryList() {
   return (
-    <div className="space-x-1">
+    <div className="grid space-y-4 justify-items-center grid-cols-2 md:grid-cols-4 xl:grid-cols-8 mt-10 ">
       {Category.map((cate) => (
-        <button className="px-2 py-1 bg-gray-400 ">{cate}</button>
+        <button className="px-2 py-1 flex  flex-col justify-center items-center bg-gray-50 h-32 w-32 rounded-full cursor-pointer hover:bg-gray-100">
+          <img
+            src={cate.image}
+            alt={cate.name}
+            className="w-20  hover:scale-105"
+          />
+          <p className="font-semibold">{cate.name}</p>
+        </button>
       ))}
     </div>
   );
