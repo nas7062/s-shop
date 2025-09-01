8597416f36d94a625011fc5e514ee0d77c7f56a3
10012 <nas7062@naver.com>
2025-09-01 21:53:28 +0900

    style : grid 반응형 적용

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,8 +7,8 @@ export default function HomePage() {
     <div className="flex flex-col gap-10">
       <Banner />
       <CategoryList />
-      <div className="grid grid-cols-6 gap-0.5">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-0.5 ">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
           <ProductCard />
         ))}
       </div>
