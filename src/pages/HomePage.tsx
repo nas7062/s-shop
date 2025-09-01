d94926e92cddee420806551b236ac361e3284241
10012 <nas7062@naver.com>
2025-09-01 22:21:30 +0900

    feat : productList

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,13 @@
 import Banner from '@/components/Banner';
 import CategoryList from '@/components/CategoryList';
-import ProductCard from '@/components/ProductCard';
+import ProductList from '@/components/ProductList';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col gap-10">
       <Banner />
       <CategoryList />
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-0.5 ">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => (
-          <ProductCard />
-        ))}
-      </div>
+      <ProductList />
     </div>
   );
 }
