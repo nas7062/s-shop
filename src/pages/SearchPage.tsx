f07ccf79e176d449be992581cf72038e3b6b89a5
10012 <nas7062@naver.com>
2025-09-06 21:35:35 +0900

    feat : 서치 페이지 input 구성

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,14 @@
+import CategoryList from '@/components/CategoryList';
 import { Search } from 'lucide-react';
 
 export default function SearchPage() {
   return (
     <div>
-      <input type="text" />
-      <Search />
+      <div className="w-full flex relative justify-center items-center ">
+        <input type="text" className="bg-gray-200 w-full rounded-md h-8" />
+        <Search className="absolute right-2" />
+      </div>
+      <CategoryList />
     </div>
   );
 }
