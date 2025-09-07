935c64924ce2cd8de373580db61575046db998f5
10012 <nas7062@naver.com>
2025-09-07 21:16:26 +0900

    style : 로그인페이지 css 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
 export default function LoginPage() {
   return (
-    <div>
-      <h2 className="bg-gray-100 py-4 text-xl font-semibold">로그인</h2>
-      <form className="flex flex-col px-4 gap-4 mt-4">
+    <div className="min-w-[320px] w-1/2 mx-auto">
+      <h2 className="bg-gray-100 py-4 text-xl font-semibold px-2">로그인</h2>
+      <form className="flex flex-col gap-4 mt-4">
         <input
           type="text"
           placeholder="아이디"
@@ -13,7 +13,9 @@ export default function LoginPage() {
           placeholder="비밀번호"
           className="border border-gray-200 px-2 py-2 rounded-md"
         />
-        <button>로그인</button>
+        <button className="border border-gray-700 bg-gray-700 hover:bg-gray-900 text-white cursor-pointer px-2 py-2 rounded-md">
+          로그인
+        </button>
       </form>
     </div>
   );
