91c140d7d8b42e2c45180323575ce77201da96ef
10012 <nas7062@naver.com>
2025-09-10 20:40:18 +0900

    refactor : ColorList  컴포넌트화

---
+++ src/components/ColorList.tsx
@@ -0,0 +1,36 @@
+import { Product } from '@/pages/DetailPage';
+import clsx from 'clsx';
+interface ColorListProps {
+  product: Product;
+  selectedColor: string | null;
+  setSelectedColor: (color: string) => void;
+}
+
+export default function ColorList({
+  product,
+  selectedColor,
+  setSelectedColor,
+}: ColorListProps) {
+  return (
+    <div className="mt-6">
+      <p className="text-sm text-gray-600 mb-2">컬러</p>
+      <div className="flex flex-wrap gap-2">
+        {product.colors.map((c) => (
+          <button
+            key={c}
+            type="button"
+            onClick={() => setSelectedColor(c)}
+            className={clsx(
+              'px-3 py-1.5 rounded-full border text-sm transition-colors cursor-pointer',
+              selectedColor === c
+                ? 'border-black'
+                : 'border-gray-300 hover:border-gray-400',
+            )}
+          >
+            {c}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -4,6 +4,7 @@ import clsx from 'clsx';
 import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductInfo from '@/components/ProductInfo';
+import ColorList from '@/components/ColorList';
 
 // 타입: 실제로는 API 응답 타입과 맞추세요
 export interface Product {
@@ -86,26 +87,11 @@ export default function DetailPage() {
         <aside className="lg:col-span-1 bg-white rounded-2xl shadow p-5 h-max lg:sticky lg:top-6">
           <ProductInfo product={product} />
           {/* 옵션: 컬러 */}
-          <div className="mt-6">
-            <p className="text-sm text-gray-600 mb-2">컬러</p>
-            <div className="flex flex-wrap gap-2">
-              {product.colors.map((c) => (
-                <button
-                  key={c}
-                  type="button"
-                  onClick={() => setSelectedColor(c)}
-                  className={clsx(
-                    'px-3 py-1.5 rounded-full border text-sm transition-colors cursor-pointer',
-                    selectedColor === c
-                      ? 'border-black'
-                      : 'border-gray-300 hover:border-gray-400',
-                  )}
-                >
-                  {c}
-                </button>
-              ))}
-            </div>
-          </div>
+          <ColorList
+            product={product}
+            selectedColor={selectedColor}
+            setSelectedColor={setSelectedColor}
+          />
 
           {/* 옵션: 사이즈 */}
           <div className="mt-4">
